8478485a33e774810ebfb6b5b03e2d791a6feeaa
Spesho-clei <jepkoechnancy56@gmail.com>
2023-02-16 08:34:26 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
+
 /**
  * main - contain char message
  * return - 1
  */
 
 int main(void)
+
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
