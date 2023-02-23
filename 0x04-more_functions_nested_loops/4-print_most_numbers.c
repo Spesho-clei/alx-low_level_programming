cf02f57c3be2c0287f50b0bd195be58090b50930
Spesho-clei <jepkoechnancy56@gmail.com>
2023-02-23 03:29:34 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ * followed by a new line
+ * return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = '0';
+
+	while
+		(i <= '9');
+	{
+		if ((i != '2' && i != '4') && i <= '9')
+			_putchar(i);
+			i++;
+		}
+	_putchar('\n');
+	return (0);
+}
