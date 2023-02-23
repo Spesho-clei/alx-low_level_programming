c3bd4c60697d620e0f78bd398d23648b15e1ea94
Spesho-clei <jepkoechnancy56@gmail.com>
2023-02-23 04:05:46 -0800

    Most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,13 @@ void print_most_numbers(void)
 {
 	int c;
 
-	c = '0';
-
-	while
-		(c <= '9');
+	for (c = 0; c <= 9; c++)
 	{
-		if ((c != '2' && c != '4') && c <= '9')
-			_putchar(c);
-		c++;
-		}
+		if ((c == 2) || (c == 4))
+			continue;
+
+		else
+			_putchar(c + '0');
+	}
 	_putchar('\n');
 }
