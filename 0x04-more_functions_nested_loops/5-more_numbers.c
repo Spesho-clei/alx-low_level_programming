33354a96ad650e67b48021cbc015855bbc9ec386
Spesho-clei <jepkoechnancy56@gmail.com>
2023-02-23 05:00:50 -0800

    Ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void)
 {
 	int c, d;
 
-	for (c = 1; c <= 10; c++)
+	for (c = 0; c <= 9; c++)
 	{
 	for (d = 0; d <= 14; d++)
 	{
-		if (d > 10)
+		if (d > 9)
 		_putchar((d / 10) + '0');
 
 		_putchar((d % 10) + '0');
