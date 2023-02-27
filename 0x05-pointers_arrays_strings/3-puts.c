0c89fe1f6f31bacf570409205645131e73ab8e0b
Spesho-clei <jepkoechnancy56@gmail.com>
2023-02-27 00:39:45 -0800

    Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ *
+ * @str: input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	while (*str)
+
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
