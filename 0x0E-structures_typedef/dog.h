760c7b734c30bf8c9a26d7cbbbc47368b57a39ef
Spesho-clei <jepkoechnancy56@gmail.com>
2023-03-20 00:47:15 -0700

    Init dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - Define a new type struct dog with the elements below
  *
@@ -16,4 +17,5 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
